5b1ccd2c574bc7f07aa2e5941dc3a454cd762d33
hainb <hainb@evolableasia.vn>
2019-12-03 08:13:28 +0700

    Commit code example junit

---
+++ src/test/java/com/example/junitest/JunitestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.junitest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JunitestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
